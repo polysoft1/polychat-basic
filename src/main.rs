a40dce673a9c292e39726085b49fb3677da1f0db
Keith Valin <keith@mailtux.net>
2022-01-24 22:10:47 -0500

    Initial program setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate polychat_core;
+
+use std::env;
+
+use polychat_core::plugin::Plugin;
+
 fn main() {
-    println!("Hello, world!");
+    let plugin_path = env::args().nth(1).expect("Usage: ./polychat-basic <absolute path to library>");
+    let loaded_plugin = &mut Plugin::new("example", &plugin_path).expect("Could not load plugin");
+
+    let acc = loaded_plugin.create_account().expect("Could not create account for plugin");
+    loaded_plugin.print(acc).expect("Could not print account");
 }
