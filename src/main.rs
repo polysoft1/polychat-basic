528f44e57d84ae139244c2e3d3bbf3bf1d20da06
Keith Valin <keith@mailtux.net>
2022-01-28 22:55:02 -0500

    Updated to use new plugin API

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     let plugin_path = env::args().nth(1).expect("Usage: ./polychat-basic <absolute path to library>");
     let loaded_plugin = &mut Plugin::new("example", &plugin_path).expect("Could not load plugin");
 
-    let acc = loaded_plugin.create_account().expect("Could not create account for plugin");
-    loaded_plugin.print(acc).expect("Could not print account");
+    let acc = loaded_plugin.create_account();
+    loaded_plugin.print(acc);
+    loaded_plugin.delete_account(acc);
 }
