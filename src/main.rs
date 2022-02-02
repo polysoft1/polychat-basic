d3d8314ef7a12fbee81069e1caf0962caf3278cb
Keith Valin <keith@mailtux.net>
2022-02-01 23:34:57 -0500

    Impelmented plugin manager creation for testing purposes

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,10 @@ extern crate polychat_core;
 
 use std::env;
 
-use polychat_core::plugin::Plugin;
+use polychat_core::plugin_manager::PluginManager;
 
 fn main() {
     env_logger::init();
-    let plugin_path = env::args().nth(1).expect("Usage: ./polychat-basic <absolute path to library>");
-    let loaded_plugin = &mut Plugin::new(&plugin_path).expect("Could not load plugin");
-
-    let acc = loaded_plugin.create_account();
-    loaded_plugin.print(acc);
-    loaded_plugin.delete_account(acc);
+    let plugin_dir_path = env::args().nth(1).expect("Usage: ./polychat-basic <absolute path to library>");
+    PluginManager::from(plugin_dir_path.as_str());
 }
