3366cf94c07bb5e1ecb21adf42c07a794b05083b
Keith Valin <keith@mailtux.net>
2022-02-14 17:36:23 -0500

    Updated test code for plugin manager

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@ use polychat_core::plugin_manager::PluginManager;
 
 fn main() {
     env_logger::init();
-    let plugin_dir_path = env::args().nth(1).expect("Usage: ./polychat-basic <absolute path to library>");
-    PluginManager::from(plugin_dir_path.as_str());
+    let plugin_dir_path = env::args().nth(1).expect("Usage: ./polychat-basic <absolute path to library folder>");
+    let mut manager = PluginManager::from(plugin_dir_path.as_str()).unwrap();
+    let services = manager.get_services();
+    for service in services {
+        let service_name = service.as_str();
+        let account = manager.create_account(service_name).unwrap();
+        manager.delete_account(service_name, account).unwrap();
+    }
 }
