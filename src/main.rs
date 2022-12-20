dd13c958f63217851f8cec6f5034b330555df1b5
Keith Valin <keith@mailtux.net>
2022-12-20 00:16:59 -0500

    Initial work towards plugin communcations with polychat-core

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 extern crate polychat_core;
+extern crate polychat_plugin;
 
 use std::env;
+use log::error;
 
-use polychat_core::plugin::Plugin;
+use polychat_core::main::Main;
+use crate::polychat_plugin::plugin::CoreInterface;
 
 fn main() {
     env_logger::init();
-    let plugin_path = env::args().nth(1).expect("Usage: ./polychat-basic <absolute path to library>");
-    let loaded_plugin = &mut Plugin::new(&plugin_path).expect("Could not load plugin");
-
-    let acc = loaded_plugin.create_account();
-    loaded_plugin.print(acc);
-    loaded_plugin.delete_account(acc);
+    let dir_path = env::args().nth(1).expect("Usage: ./polychat-basic <absolute path to library>");
+    let mut main = Main::new();
+    
+    match main.init(&dir_path) {
+        Err(e) => error!("Error loading plugin directory, {}", e),
+        Ok(_) => main.test("We good!".to_string())
+    };
 }
