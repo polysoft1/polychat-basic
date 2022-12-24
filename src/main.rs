703fc5a87a1c6c01e1ba959e14156c778362038c
Keith Valin <keith@mailtux.net>
2022-12-23 19:17:27 -0500

    Remove compiler errors

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ extern crate polychat_plugin;
 use std::env;
 use log::{error, info};
 
-use polychat_core::main::Main;
+use polychat_core::core::Core;
 
 fn main() {
     env_logger::init();
     let dir_path = env::args().nth(1).expect("Usage: ./polychat-basic <absolute path to library>");
-    let mut main = Main::new();
+    let mut main = Core::new();
     
     match main.init(&dir_path) {
         Err(e) => error!("Error loading plugin directory, {}", e),
